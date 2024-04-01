06bb2c0a007379c189a8c6bbe86b305044c70916
NextFire <git@yuru.moe>
2024-04-01 12:05:10 -0400

    merge overlay files

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,11 @@
 { inputs, ... }: {
-  imports = [
-    inputs.flake-parts.flakeModules.easyOverlay
-    ./pipx.nix
-  ];
+  imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+  perSystem = { pkgs, lib, ... }: {
+    overlayAttrs = {
+      # FIXME: https://github.com/NixOS/nixpkgs/pull/298528
+      pipx = pkgs.pipx.overrideAttrs (oldAttrs: {
+        disabledTests = oldAttrs.disabledTests ++ [ "test_skip_maintenance" ];
+      });
+    };
+  };
 }
