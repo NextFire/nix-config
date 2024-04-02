8632ed17d0f2fee7ed589835e7979185e054da99
NextFire <git@yuru.moe>
2024-04-02 13:44:41 -0400

    remove broken aegisub

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,6 @@
 {
   perSystem = { pkgs, ... }: {
     packages = {
-      aegisub-japan7 = import ./japan7/aegisub.nix pkgs;
       amaranth = import ./fonts/amaranth.nix pkgs;
       cookhand-std-r = import ./fonts/cookhand-std-r pkgs;
       syncplay-japan7 = import ./japan7/syncplay.nix pkgs;
