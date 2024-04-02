12e8fef46d9040a4a49bd4e7d220567fbe13aad2
NextFire <git@yuru.moe>
2024-04-02 19:51:50 -0400

    helmfile devshell

--- shells/devshells.nix
+++ shells/devshells.nix
@@ -1,4 +1,22 @@
 { pkgs, ... }: {
+  helmfile = pkgs.mkShell {
+    packages =
+      let
+        kubernetes-helm-wrapped = with pkgs; wrapHelm kubernetes-helm {
+          plugins = with pkgs.kubernetes-helmPlugins; [
+            helm-diff
+            helm-git
+            helm-s3
+            helm-secrets
+          ];
+        };
+        helmfile-wrapped = pkgs.helmfile-wrapped.override {
+          inherit (kubernetes-helm-wrapped) pluginsDir;
+        };
+      in
+      [ kubernetes-helm-wrapped helmfile-wrapped ];
+  };
+
   rust = pkgs.mkShell {
     packages = with pkgs; [
       fenix.stable.minimalToolchain
