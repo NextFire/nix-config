29bcae1e766d74979a54e89625c4246672caf982
NextFire <git@yuru.moe>
2024-04-06 19:18:02 -0400

    unbloat devshell template

--- shells/template/flake.nix
+++ shells/template/flake.nix
@@ -4,18 +4,11 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, self, ... }:
+  outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-      perSystem = { pkgs, system, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-          ];
-        };
-
+      perSystem = { pkgs, ... }: {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
           ];
